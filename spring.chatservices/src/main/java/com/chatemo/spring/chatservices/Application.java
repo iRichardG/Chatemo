2a023554f9c21b8aff08b691788448482445f440
Richard Gonzalez <richardgonzalez@Richards-MacBook-Pro.local>
2020-08-12 11:12:22 -0500

    adding repo

---
+++ spring.chatservices/src/main/java/com/chatemo/spring/chatservices/Application.java
@@ -0,0 +1,13 @@
+package com.chatemo.spring.chatservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
