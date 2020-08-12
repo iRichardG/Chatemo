674bc0a82b643d2effbf0eba8aef873d59a49bcf
RyanHowe083 <ryan.howe2008@gmail.com>
2020-08-12 13:16:08 -0500

    Fixing build path

--- spring.chatservices/src/main/java/com/chatemo/spring/chatservices/controller/MessageController.java
+++ spring.chatservices/src/main/java/com/chatemo/spring/chatservices/controller/MessageController.java
@@ -1,12 +1,15 @@
-package com.chatemo.spring.controllerservice;
+package com.chatemo.spring.chatservices.controller;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class Controllers {
+@RestController
+public class MessageController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/hello")
 	public @ResponseBody String greeting() {
 		
 		return "Hello World";
